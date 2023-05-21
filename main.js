3062e84cf3f40dd3693fadddd57ed3b9eaac9a5c
pears <pears@BENS-PC>
2023-05-21 19:20:17 +0100

    Added service worker.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const locationClassName = "cGDiWU3FlTjqSs-F1LwK4";
 const distanceClassName = "_1ZY603T1ryTT3dMgGkM7Lg";
 const spanContainerClassName = "mlEuHXZpfrrzJtwlRmwBe";
 
+
 const createDistanceElementTemplate = (name, distance, imgSrc) => 
     `<li>
         <div class="${iconClassName}">
@@ -37,6 +38,51 @@ const init = () => {
 };
 
 init();
+
+
+
+
+
+
+
+
+// import {default as classes} from "RightmoveClassNames.js";
+
+// const createDistanceElementTemplate = (name, distance, imgSrc) => 
+//     `<li>
+//         <div class="${classes.icon}">
+//             <svg role="img" data-testid="svg-nationalrail" aria-hidden="true"><use xlink:href="#nationalrail"></use></svg>
+//         </div>
+//         <div class="${classes.distContainer}">
+//             <div class="${classes.spanContainer}">
+//                 <span class="${classes.locSpan}">${name}</span>
+//                 <span class="${classes.distSpan}">${distance} miles</span>
+//             </div>
+//         </div>
+//     </li>`;
+
+// const init = () => {
+  
+//   const ul = document.getElementsByClassName(classes.ul)[0];
+
+//   // If no matches, return early.
+//   if (ul.length === 0) return;
+
+
+//   let testElements = [
+//     createDistanceElementTemplate("Eloons Gaystation", "0.3", ""),
+//     createDistanceElementTemplate("Denbigh Lidl", "0.5", ""),
+//     createDistanceElementTemplate("Pet Shop", "0.1", ""),
+//     createDistanceElementTemplate("Rhyl", "2.9", ""),
+//   ];
+
+//   testElements.forEach(x => ul.insertAdjacentHTML("beforeend", x))
+// };
+
+// init();
+
+
+
 
 
 
---
+++ rightmoveClassNames.js
@@ -0,0 +1,30 @@
+// /**
+//  * Collection of obfuscated HTML classnames used by rightmove.co.uk.
+//  */
+// export default RightmoveClassNames = {
+//     /**
+//      * Target icon class.
+//      */
+//     icon: "_33FcKz0Izh9IdGj_vDDgmK",
+//     /**
+//      * Target <ul> parent classname.
+//      */
+//     ul: "_2f-e_tRT-PqO8w8MBRckcn",
+//     /**
+//      * Distance container classname.
+//      */
+//     distContainer: "Hx6myckw6FR-gq2-nskGM",
+//     /**
+//      * Distance span container classname.
+//      */
+//     spanContainer: "mlEuHXZpfrrzJtwlRmwBe",
+//     /**
+//      * Location span classname.
+//      */
+//     locSpan: "cGDiWU3FlTjqSs-F1LwK4",
+//     /**
+//      * Distance span classname.
+//      */
+//     distSpan: "Hx6myckw6FR-gq2-nskGM",
+// };
+
