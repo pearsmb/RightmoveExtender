702fdb2443c33349e35f8dea14cba5bd3a38c9b4
pears <pears@BENS-PC>
2023-05-21 18:51:08 +0100

    Initial commit.

---
+++ main.js
@@ -0,0 +1,42 @@
+const iconClassName = "_33FcKz0Izh9IdGj_vDDgmK";
+const ulClassName = "_2f-e_tRT-PqO8w8MBRckcn";
+const distanceElementsClassName = "Hx6myckw6FR-gq2-nskGM";
+const locationClassName = "cGDiWU3FlTjqSs-F1LwK4";
+const distanceClassName = "_1ZY603T1ryTT3dMgGkM7Lg";
+const spanContainerClassName = "mlEuHXZpfrrzJtwlRmwBe";
+
+const createDistanceElementTemplate = (name, distance, imgSrc) => 
+    `<li>
+        <div class="${iconClassName}">
+            <svg role="img" data-testid="svg-nationalrail" aria-hidden="true"><use xlink:href="#nationalrail"></use></svg>
+        </div>
+        <div class="${distanceElementsClassName}">
+            <div class="${spanContainerClassName}">
+                <span class="${locationClassName}">${name}</span>
+                <span class="${distanceClassName}">${distance} miles</span>
+            </div>
+        </div>
+    </li>`;
+
+const init = () => {
+  
+  const ul = document.getElementsByClassName(ulClassName)[0];
+
+  // If no matches, return early.
+  if (ul.length === 0) return;
+
+
+  let testElements = [
+    createDistanceElementTemplate("Eloons Gaystation", "0.3", ""),
+    createDistanceElementTemplate("Denbigh Lidl", "0.5", ""),
+    createDistanceElementTemplate("Pet Shop", "0.1", ""),
+    createDistanceElementTemplate("Rhyl", "2.9", ""),
+  ];
+
+  testElements.forEach(x => ul.insertAdjacentHTML("beforeend", x))
+};
+
+init();
+
+
+
